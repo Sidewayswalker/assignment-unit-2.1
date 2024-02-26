4d99eb0eed61f149e81b0c179b5659b2559083c9
Steven Karl <Karl.steven09@gmail.com>
2024-02-26 12:53:19 -0600

    everything down to FriendsPets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,32 +1,41 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Steven ';
+console.log('My First Name is', firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Karl';
+console.log('My Last Name is', lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = firstName + lastName;
+console.log('My Full Name is', fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-
+let luckyNumber = 9;
+console.log('My Lucky Number is', luckyNumber);
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
-
+let introduction = 'My name is ' + fullName + ' and I think ' + luckyNumber + ' is a winner!'
+console.log(introduction);
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
-
+let adventurous = true;
+console.log('Am I Adventurous?', adventurous);
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
-
+let food = 'My favorite food is an apple a day. I hear it keeps the doctor away.'
+console.log(food);
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
-
+let pets = 2
+console.log('Amount of pets that I have', pets);
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 3;
+console.log('How many pets does your friend have', friendsPets);
 // 10 - Add two pets to your `pets` variable
 
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
